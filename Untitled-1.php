0412f795f9b4c6f2438e0d0a056ac3458fa19fc0
mohamad <mohamadassaf79@gmail.com>
2021-12-08 11:17:44 +0100

    Ajouté secret

---
+++ Untitled-1.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html>
+
+<header>
+   <nav>
+    <ul id="navbar">
+       <li class="navbar-elt">Accueil</li>
+       <li class="navbar-elt">Articles</li>
+       <li class="navbar-elt">Images</li>
+       <li class="navbar-elt">Contact</li>
+  </ul>
+ </nav>
+ </header>
+
+
+ <section id="section-content">
+  <div class="zone-paragraphe">
+    <p>Lorem zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz
+        zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz
+        zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz
+    </p>
+    
+    <p>Lorem aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+        aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+        aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+    </p>
+ </div>
+ <img src=*../fonds d'écran /
+
+ </section>
+
+ <section id="section-form">
+    <form action="/my-handling-form-page"method="post" style="display: flex; flex-direction: column;">
+
+        <label for="fname">Prénom:</label>
+        <input type="text" id="fname" name="fname"><br><br>
+        <label for="lname">Nom:</label>
+        <input type="text" id="lname" name="lname"><br><br>
+        <label for="fname">Voie:</label>
+        <input type="text" id="fname" name="fname"><br><br>
+        <label for="fname">Code postal:</label>
+        <input type="text" id="fname" name="fname"><br><br>
+        <label for="fname">Ville:</label>
+        <input type="text" id="fname" name="fname"><br><br>
+        <input type="submit" value="Submit">
+      </form>
+ </section>
+
+ <footer>
+    <p>je suis un pied de page</p>
+ </footer>
+
+</html>
